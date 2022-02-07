2a285aaf00f8e79e3dfa5509837b1d09a7cb6b76
shoneriki <ikirenohs@gmail.com>
2022-02-07 14:26:06 +0900

    changed form and put form in application view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,14 @@ class PagesController < ApplicationController
   end
 
   def contact
-    @members = {"Frodo Baggins" => {name: "Frodo", phone: 123_1234},"Samwise Gamgee" => {name: "Sam", phone: 123_1234, }}
+    search = params[:member]
+    @members = {"Frodo" => {name: "Frodo Baggins", dependability: 5},"Sam" => {name: "Samwise Gamgee", dependability: 10 }, "Gandalf" => {name: "Gandalf The White", dependability: 10}, "Aragorn" => {name: "Aragorn", dependability: 10}}
 
+    if search.present?
+      @members = @members.select { |name|  name.downcase == search.downcase }
+    else
+      @members = {"Frodo" => {name: "Frodo Baggins", dependability: 5},"Sam" => {name: "Samwise Gamgee", dependability: 10 }, "Gandalf" => {name: "Gandalf The White", dependability: 10}, "Aragorn" => {name: "Aragorn", dependability: 10}}
+    end
   end
 
   def home
